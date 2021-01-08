098d3bf66e834eaecd67df098b99ba786a53d157
TtBbSs <46806848+TtBbSs@users.noreply.github.com>
2021-01-08 13:40:43 +0100

    Add files via upload

---
+++ smashing-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	Plugin Name: A - TBS
+	Description: This is for updating your Wordpress plugin.
+	Version: 1.0.0
+	Author: TBS
+	Author URI: http://www.matthewray.com
+*/
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+$updater = new Smashing_Updater( __FILE__ );
+$updater->set_username( 'TtBbSs' );
+$updater->set_repository( 'smashing-plugin' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize();
